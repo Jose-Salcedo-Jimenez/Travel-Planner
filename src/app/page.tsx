e0999a9e87fe2cb893b5f7bd055cc9c071224cc2
Jose Salcedo Jimenez <josesslcedo@gmail.com>
2025-10-22 08:32:11 -0500

    First commit main view model completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,44 @@
 import Image from "next/image";
-
+import { Playfair_Display } from "next/font/google";
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-playfair",
+});
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <div className="relative w-full h-screen ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src={"/Fondo.png"}
+          alt="Imagen de cartagena de fondo"
+          fill
+          className="object-cover -z-10"
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+        <main className="w-full h-full bg-black/45 p-12 ">
+          <div className="w-1/2 h-full flex flex-col justify-center gap-4">
+            <p
+              className={`${playfairDisplay.className} italic font-light text-lg text-[#E7DECC]`}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              Lorem ipsum dolor
+            </p>
+            <h1
+              className={`${playfairDisplay.className} normal font-normal text-[55px]/17 text-[#E7DECC] tracking-[.8] `}
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+              Cartagena, like you have{" "}
+              <span className="italic">never herd about</span>
+            </h1>
+            <a
+              href="#"
+              className={`${playfairDisplay.className} w-[15vw] bg-[#E7DECC] text-sm tracking-[1] text-[#56585B] text-center p-3`}
+            >
+              Star planning your trip
+            </a>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
